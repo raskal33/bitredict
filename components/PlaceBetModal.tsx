b8dc8a7e1f83f148f58413ef5b5cab6b3b76dd2b
raskal33 <coinologist33@gmail.com>
2025-11-01 01:01:42 +0300

    fix: Remove unused ArrowTrendingUpIcon import

--- components/PlaceBetModal.tsx
+++ components/PlaceBetModal.tsx
@@ -6,8 +6,7 @@ import {
   XMarkIcon,
   BoltIcon,
   CheckCircleIcon,
-  ExclamationCircleIcon,
-  ArrowTrendingUpIcon
+  ExclamationCircleIcon
 } from "@heroicons/react/24/outline";
 import { useAccount } from "wagmi";
 import { useRouter } from "next/navigation";
